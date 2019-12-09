969360b7cab893df43a803a7b44c7f2a90d3ce9d
soeren <56670304+soerenschneider@users.noreply.github.com>
2019-12-10 00:48:16 +0100

    Added day 09 zzzZZZzzz

--- main.go
+++ main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"github.com/soerenschneider/adventofcode2019/day01"
-	"github.com/soerenschneider/adventofcode2019/day02"
-	"github.com/soerenschneider/adventofcode2019/day03"
-	"github.com/soerenschneider/adventofcode2019/day04"
-	"github.com/soerenschneider/adventofcode2019/day05"
-	"github.com/soerenschneider/adventofcode2019/day06"
-	"github.com/soerenschneider/adventofcode2019/day08"
+	"github.com/soerenschneider/adventofcode2019/day09"
 )
 
 func main() {
-	day01.Answer()
-	fmt.Println("------------")
-	day02.Answer()
-	fmt.Println("------------")
-	day03.Answer03()
-	fmt.Println("------------")
-	day04.Answer04()
-	fmt.Println("------------")
-	day05.Answer05()
-	fmt.Println("------------")
-	day06.Answer06()
-	fmt.Println("------------")
-	day08.Answer08()
-	fmt.Println("------------")
+	day09.Answer09()
+	return
 }
+
