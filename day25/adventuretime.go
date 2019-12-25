32008a953edfc103f0acb283809c7c5e2f659172
soeren <56670304+soerenschneider@users.noreply.github.com>
2019-12-25 15:24:45 +0100

    Added day 25

---
+++ day25/adventuretime.go
@@ -0,0 +1,51 @@
+package day25
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/soerenschneider/adventofcode2019/util"
+	"os"
+	"strings"
+)
+
+const done = '\n'
+
+func readInput(in chan int64) {
+	reader := bufio.NewScanner(os.Stdin)
+	for reader.Scan() {
+		for _, r := range reader.Bytes() {
+			in <- int64(r)
+		}
+		in <- int64(done)
+	}
+}
+
+func Answer25() int64 {
+	input := util.ReadInt64Array("resources/day25/input.txt")
+	
+	in := make(chan int64)
+	out := make(chan int64)
+	interpreter := util.NewInterpreter(input, in, out)
+	
+	go interpreter.Execute()
+	go readInput(in)
+
+	var buffer strings.Builder
+	for o := range out {
+		if o == done {
+			fmt.Println(buffer.String())
+			buffer.Reset()
+		} else if isAscii(o) {
+			buffer.WriteByte(byte(o))
+		} else {
+			fmt.Println(buffer.String())
+			return o
+		}
+	}
+	return -1
+}
+
+func isAscii(o int64) bool {
+	return o >= 0 && o <= 128
+}
+
