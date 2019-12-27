456af1ff9a48c4df0cfbac5d0e72e46189f2f282
soeren <56670304+soerenschneider@users.noreply.github.com>
2019-12-27 12:16:18 +0100

    Added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# solutions for advent of code 2019
+ 
+> You take the escape pod. 
+> 
+> You're launched into space! Bye!
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/soerenschneider/adventofcode2019/day10"
-)
-
-func main() {
-	day10.Answer10()
-}
